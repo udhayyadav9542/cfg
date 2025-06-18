76e050745033af876e34f3a61bd41e077837d29e
srimanikantaBattu <srimanikantabattu04@gmail.com>
2025-06-18 20:55:56 +0530

    Changed in manikanta branch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Changed Some Code here !
       </p>
     </>
   )
